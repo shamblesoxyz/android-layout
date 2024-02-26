044f060135ebc70ad846733258fba12013af5ed4
Trong Phuc <phuc86119@gmail.com>
2024-02-26 22:33:23 +0700

    init

---
+++ app/src/main/java/com/practice/layout/LinearLayoutActivity.java
@@ -0,0 +1,14 @@
+package com.practice.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinearLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layout);
+    }
+}
